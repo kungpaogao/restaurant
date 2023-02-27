44d9b3f021af66528f677d7e124326a966970a95
kungpaogao <kungpaogao@gmail.com>
2023-02-27 00:17:18 -0500

    Update gradle and gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
 
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.0-alpha11").apply(false)
-    id("com.android.library").version("8.0.0-alpha11").apply(false)
+    id("com.android.application").version("8.1.0-alpha06").apply(false)
+    id("com.android.library").version("8.1.0-alpha06").apply(false)
     kotlin("android").version("1.7.10").apply(false)
     kotlin("multiplatform").version("1.7.10").apply(false)
 }
